394045a1abc9ba9fd7cc53abe94d0d37b5c970f0
Matteo Sandrin <matteo.sandrin@hotmail.it>
2023-05-19 11:40:07 -0400

    Run linter on "chore_coat.py"

--- in-stock/chore_coat.py
+++ in-stock/chore_coat.py
@@ -14,33 +14,47 @@ if not os.path.exists(CONFIG_PATH):
 
 CONFIG = json.load(open(CONFIG_PATH))
 
+
 def notify(message):
     params = {
-        "token" : CONFIG["pushover_token"],
-        "user" : CONFIG["pushover_user"],
-        "message" : message
+        "token": CONFIG["pushover_token"],
+        "user": CONFIG["pushover_user"],
+        "message": message,
     }
     requests.post("https://api.pushover.net/1/messages.json", params=params)
 
-print("Downloading \"{}\" ...".format(URL))
+
+print('Downloading "{}" ...'.format(URL))
 response = requests.get(
     URL,
     headers={
-        "user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-    })
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    },
+)
 
 if response.status_code != 200:
-    print("ERROR: {} replied with status code {}".format(URL, response.status_code), file=sys.stderr)
+    print(
+        "ERROR: {} replied with status code {}".format(URL, response.status_code),
+        file=sys.stderr,
+    )
     exit(1)
 
 re_sold_out = re.search(r"<button.*ProductForm__AddToCart.*Sold Out", response.text)
-re_available = re.search(r"<button.*ProductForm__AddToCart.*Add to basket", response.text)
+re_available = re.search(
+    r"<button.*ProductForm__AddToCart.*Add to basket", response.text
+)
 
 if (re_sold_out is None) and (re_available is None):
-    print("ERROR: neither \"sold out\" or \"available\" regular expressions were matched", file=sys.stderr)
+    print(
+        'ERROR: neither "sold out" or "available" regular expressions were matched',
+        file=sys.stderr,
+    )
     exit(1)
 if (re_sold_out is not None) and (re_available is not None):
-    print("ERROR: both \"sold out\" and \"available\" regular expressions were matched", file=sys.stderr)
+    print(
+        'ERROR: both "sold out" and "available" regular expressions were matched',
+        file=sys.stderr,
+    )
     exit(1)
 
 if re_sold_out is not None:
